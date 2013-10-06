9d69b5e9dee0d606a82952c0ef9a56a179f8485c
EnigMoiD <evan.dorsky@icloud.com>
2013-10-06 14:34:55 -0400

    Basic views and serial intercommunication

--- arduino/main.ino
+++ arduino/main.ino
@@ -12,42 +12,41 @@ float rawRange = 1024; // 3.3v
 float logRange = 5.0; // 3.3v = 10^5 lux
 
 float processedLux() {
-  return rawToLux(analogRead(sensorPin));
+	return rawToLux(analogRead(sensorPin));
 }
 
 float rawToLux(int raw)
 {
-  float logLux = raw * logRange / rawRange;
-  return pow(10, logLux);
+	float logLux = raw * logRange / rawRange;
+	return pow(10, logLux);
 }
 
 void setup()
 {
-  analogReference(EXTERNAL);
-  Serial.begin(9600);
+	analogReference(EXTERNAL);
+	Serial.begin(9600);
 
-  for (int i = 0; i < servoCount; i++) {
-    servo.attach(servoPin);
-  }
-}
-
-void loop() {
-    loopServo();
+	servo.attach(servoPin);
 }
 
 void loopServo() {
-  for(pos = 0; pos < 180; pos += 1) {
-    for (int i = 0; i < servoCount; i++) {
-      servos[i].write(pos);
-      printPoint(averagedRange(10), pos, 2);
-    }
-    delay(15);
-  }
-  for(pos = 180; pos>=1; pos-=1) {
-    for (int i = 0; i < servoCount; i++) {
-      servos[i].write(pos);
-      printPoint(averagedRange(10), pos, 2);
-    }
-    delay(15);
-  }
+	for(pos = 0; pos < 180; pos += 1) {
+		servo.write(pos);
+		delay(15);
+	}
+	for(pos = 180; pos>=1; pos-=1) {
+		servo.write(pos);
+		delay(15);
+	}
+}
+
+void loop() {
+	if (Serial.available() > 0) {
+		char incomingByte = Serial.read();
+
+		Serial.print("I received: ");
+		Serial.println(incomingByte);
+	}
+	
+	loopServo();
 }
--- node/app.js
+++ node/app.js
@@ -1,8 +1,59 @@
-console.log("boots");
+var
+serialport = require('serialport'),
+express = require('express'),
+stylus = require('stylus'),
+nib = require('nib');
 
-var SerialPort = require("serialport").SerialPort;
-console.log("1=============");
-console.log(SerialPort);
-var serialPort = new SerialPort("/dev/tty.Bluetooth-Modem", {
-  baudrate: 9600
+// App
+var app = express();
+app.configure(function() {
+	app.use(stylus.middleware({
+			src: __dirname + '/views',
+			dest: __dirname + '/app',
+			compile: compile,
+			force: true
+		})
+	);
+	app.use(express.static(__dirname + '/app'));
 });
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+// Stylus
+function compile(str, path) {
+	return stylus(str)
+		.set('filename', path)
+		.use(nib());
+}
+
+// Serial
+var SerialPort = serialport.SerialPort;
+
+var serialPort = new SerialPort("/dev/tty.usbmodem1421", {
+	baudrate: 9600,
+	parser: serialport.parsers.readline("\n")
+});
+
+serialPort.on('open', function () {
+	serialPort.on('data', function(data) {
+		console.log('data: ' + data);
+	});
+});
+
+// Routes
+app.get('/', function (req, res) {
+	console.log('home!');
+	res.render('index', {
+		title : 'Home'
+	});
+});
+
+app.get('/serial', function (req, res) {
+	console.log('cereal!');
+	serialPort.write('Words!\n', function(err, results) {
+		console.log('err: ' + err);
+		console.log('results: ' + results);
+	});
+});
+
+app.listen(process.env.PORT || 5000);
