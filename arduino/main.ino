c35001713e03cdbc8503d0e96de618bf3937c758
c-bailey <xxcbaileyxx@yahoo.com>
2013-10-06 19:55:07 -0400

    updated syntax

--- arduino/main.ino
+++ arduino/main.ino
@@ -4,9 +4,8 @@
 Servo servo;
 int servoPin = 9;
 int pos = 0;
-int dishAngle = [10,30,40,60];
-int dishLED = [1,2,3,4];
-
+int dishAngle[] = {10,30,40,60};
+int dishLED[] = {1,2,3,4};
 // Sensor variables
 int sensorPin = A0;
 
@@ -31,19 +30,40 @@ void setup()
 	servo.attach(servoPin);
 }
 
-void observeDish(incomingByte) {
-	servo.write(dishAngle[incomingByte])
-	
+float averagedRange(int count) {
+  float total = 0;
+
+  for (int i = 0; i < count; i ++) {
+    float sensorValue = processedLux();
+    total += sensorValue;
+
+    delay(2);
+  }
+
+  return total/count;
+}
+
+float observeDish(int dishNum) {
+	servo.write(dishAngle[dishNum]);
+	float opacity= averagedRange(20);
+	Serial.print(dishNum);
+	Serial.print("=");
+	Serial.println(opacity);
+
 }
 
 void loop() {
-	if (Serial.available() > 0) {
-		char incomingByte = Serial.read();
 
-		Serial.print("I received: ");
-		Serial.println(incomingByte);
+//	if (Serial.available() > 0) {
+//		char incomingByte = Serial.read();
+
+	//	Serial.print("I received: ");
+	//	Serial.println(incomingByte);
+		
+	//}
+	for (int i=0; i<4; i++) {
+		int incomingByte=i;
 		observeDish(incomingByte);
 	}
-	
 	
 }
