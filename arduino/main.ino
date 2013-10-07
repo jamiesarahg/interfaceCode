2556df655643ba2d0048bd7bd3f72c060755c1da
c-bailey <xxcbaileyxx@yahoo.com>
2013-10-06 20:25:03 -0400

    added LED control

--- arduino/main.ino
+++ arduino/main.ino
@@ -4,10 +4,12 @@
 Servo servo;
 int servoPin = 9;
 int pos = 0;
-int dishAngle[] = {10,30,40,60};
-int dishLED[] = {1,2,3,4};
 // Sensor variables
 int sensorPin = A0;
+// Set values
+int dishAngle[] = {0,30,60,90};
+// LEDS
+int leds[] = {13,12,11,10};
 
 float rawRange = 1024; // 3.3v
 float logRange = 5.0; // 3.3v = 10^5 lux
@@ -28,6 +30,10 @@ void setup()
 	Serial.begin(9600);
 
 	servo.attach(servoPin);
+
+	for (int i=0; i<4; i++) {
+		pinMode(leds[i], OUTPUT); 
+	}
 }
 
 float averagedRange(int count) {
@@ -44,11 +50,14 @@ float averagedRange(int count) {
 }
 
 float observeDish(int dishNum) {
+	digitalWrite(leds[dishNum], HIGH); 
 	servo.write(dishAngle[dishNum]);
+	delay(15);
 	float opacity= averagedRange(20);
 	Serial.print(dishNum);
 	Serial.print("=");
 	Serial.println(opacity);
+	digitalWrite(leds[dishNum], LOW); 
 
 }
 
@@ -59,9 +68,11 @@ void loop() {
 
 	//	Serial.print("I received: ");
 	//	Serial.println(incomingByte);
-		
+	
+	//	observeDish(incomingByte);	
 	//}
-	for (int i=0; i<4; i++) {
+
+	for (int i=0; i<4; i++) {	//for testing w/out serial
 		int incomingByte=i;
 		observeDish(incomingByte);
 	}
